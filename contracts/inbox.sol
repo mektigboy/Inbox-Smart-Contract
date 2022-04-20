dd71c02d27dc9b196611663d13f5fabd88d200ff
Ángel Sánchez <mektigboy@protonmail.com>
2022-04-20 18:59:26 +0000

    Update inbox.sol

--- contracts/inbox.sol
+++ contracts/inbox.sol
@@ -1,9 +1,9 @@
-pragma solidity ^0.4.26;
+pragma solidity ^0.8.13;
 
 contract Inbox {
   string public message;
 
-  function Inbox(string initialMessage) public {
+  constructor Inbox(string initialMessage) public {
     message = initialMessage;
   }
   function setMessage(string newMessage) public {
