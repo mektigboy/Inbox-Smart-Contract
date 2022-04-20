35c37741820e21d8b82060db9604a490ade65cb9
Ángel Sánchez <mektigboy@protonmail.com>
2022-04-20 16:10:37 +0000

    Update package: solc | Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
-const { interface, byteface } = require('./complile');
+const { interface, bytecode } = require('./compile');
 const provider = new HDWalletProvider(
   // This seed phrase is from a test wallet.
   // Use environment variables when using a real wallet.
@@ -8,3 +8,20 @@ const provider = new HDWalletProvider(
   'https://rinkeby.infura.io/v3/cbe42a9daef64613bff25927292e1666'
 );
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from accounts', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+  .deploy({ data: bytecode, arguments: ['Hi, there!'] })
+  .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+
+  //To prevent a hanging deployment.
+  provider.engine.stop();
+};
+
+deploy();
