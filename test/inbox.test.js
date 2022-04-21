9be6326824874a075b1aca08d9eb0fdb28e077c5
Ángel Sánchez <mektigboy@protonmail.com>
2022-04-21 03:23:00 +0000

    Update inbox.test.js

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const ganache = require('ganache-cli');
+const ganache = require('ganache');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 const { abi, evm } = require('../compile');
@@ -10,7 +10,6 @@ let inbox;
 beforeEach(async () => {
   // Get a list of all accounts.
   accounts = await web3.eth.getAccounts();
-
   // Use one of those accounts to deploy the contract.
   inbox = await new web3.eth.Contract(abi)
     .deploy({
